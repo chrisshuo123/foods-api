7d2b4677174cd12aa0557b85b7e28b456736a22c
chrisshuo123 <chr1z100@gmail.com>
2026-07-30 19:05:15 +0700

    add var  set to true, while add  array with 'other_location' column, complete with relations method towards the 4 state database and a method to slice and lower case the states from the database rows

--- app/Models/Food.php
+++ app/Models/Food.php
@@ -7,6 +7,28 @@ use Illuminate\Database\Eloquent\Model;
 class Food extends Model
 {
     protected $table = 'foods';
+    public $timestamps = true;
     
-    protected $fillable = ['name', 'image', 'slug', 'body'];
+    protected $fillable = ['name', 'image', 'other_location', 'slug', 'body'];
+
+    public function cleanLocationName($name) {
+        // Hilangkan Kab. Kota
+        $name = preg_replace('/^(kab\.|kota)\s*/i', '', $name);
+        // Ubah huruf kecil semua
+        $name = strtolower($name);
+        return ucwords($name); // ucwords method untuk memberi initial caps pada tiap kata.
+    }
+
+    public function province() {
+        return $this->belongsTo(RegProvince::class, 'province_id');
+    }
+    public function regency() {
+        return $this->belongsTo(RegRegencies::class, 'regency_id');
+    }
+    public function district() {
+        return $this->belongsTo(RegDistricts::class, 'district_id');
+    }
+    public function village() {
+        return $this->belongTo(RegVillages::class, 'village_id');
+    }
 }
