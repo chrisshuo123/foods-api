f4908ae0b8dd43a39c364d657c3d5a3da95c40a5
chrisshuo123 <chr1z100@gmail.com>
2026-07-30 19:13:57 +0700

    add toTitleCase and cleanLocationName method for the Foods Origin output, while add 3 const vars in line 18-20 after adding 3 metadatas in food.blade.php, while giving if command logics for Food Location origin output.

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,14 +1,41 @@
+// Setiap awal sebuah kata diawali dengan Caps
+function toTitleCase(str) {
+    return str.replace(/\b\w/g, char => char.toUpperCase());
+}
+
+// menghapus kab., kota, dan mengatur Lowercase pada function javascript
+function cleanLocationName(name) {
+    return toTitleCase (
+        name.replace(/^(kab\.|kota)\s*/i, '').toLowerCase()
+    );
+}
+
 function openModal(element) {
     // Ambil data dari attribute
     const name = element.dataset.name;
     const image = element.dataset.image;
     const body = element.dataset.body;
+    const regency = element.dataset.regency;
+    const province = element.dataset.province;
+    const otherLocation = element.dataset.other_location;
 
     // Isi ke modal
     document.getElementById('modalTitle').textContent = name;
     document.getElementById('modalImage').src = '/img/' + image;
     document.getElementById('modalImage').alt = name;
     document.getElementById('modalBody').textContent = body;
+
+    // Location Logic
+    let locationText = 'Lokasi Tidak Tersedia';
+    if(regency && province) {
+        locationText = cleanLocationName(regency) + ', ' + cleanLocationName(province) + ', Indonesia';
+    } else if (province) {
+        locationText = cleanLocationName(province) + ', Indonesia';
+    } else if(otherLocation) {
+        locationText = otherLocation;
+    }
+    document.getElementById('modalLocation').textContent = locationText;
+
     // Tampilkan Modal
     document.getElementById('myModal').style.display = 'block';
 }
