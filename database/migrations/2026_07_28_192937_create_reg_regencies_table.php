0b977c5843df38835c7e9b2e7444425121471179
chrisshuo123 <chr1z100@gmail.com>
2026-07-30 18:58:43 +0700

    add new migrations table for list of states database to order foods origin

---
+++ database/migrations/2026_07_28_192937_create_reg_regencies_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reg_regencies', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('province_id')->constrained(
+                table: 'reg_provinces',
+                indexName: 'regencies_provinces_id'
+            );
+            $table->string('name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reg_regencies');
+    }
+};
